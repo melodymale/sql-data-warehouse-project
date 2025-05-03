a0016a47ae34786371b2e0f1da28098777fc8c8a
chayutpong <chayutpong.pro@gmail.com>
2025-05-03 19:45:51 +0700

    fix: add trim in case when for country

--- scripts/silver/full_load_silver.sql
+++ scripts/silver/full_load_silver.sql
@@ -147,9 +147,9 @@ INSERT INTO silver.erp_loc_a101 (
 
 SELECT REPLACE(cid, '-', '') AS cid,
 CASE
-	WHEN cntry = 'DE' THEN 'Germany'
-	WHEN cntry IN ('US', 'USA') THEN 'United States'
-	WHEN TRIM(cntry) = '' THEN 'n/a'
+	WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
 	ELSE TRIM(cntry)
 END AS cntry
 FROM bronze.erp_loc_a101;
