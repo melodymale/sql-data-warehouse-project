20d05a3a376d1971030e65c291cb3fc58ecaacec
Chayutpong Promya <31468410+melodymale@users.noreply.github.com>
2025-05-03 19:46:59 +0700

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,6 +1,25 @@
-DROP VIEW IF EXISTS gold.dim_customer;
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
 
-CREATE VIEW IF NOT EXISTS gold.dim_customer AS
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+DROP VIEW IF EXISTS gold.dim_customers;
+
+CREATE VIEW gold.dim_customers AS
 SELECT ROW_NUMBER() OVER(ORDER BY ci.cst_id) AS customer_key,
 ci.cst_id AS customer_id,
 ci.cst_key AS customer_number,
@@ -19,3 +38,56 @@ LEFT JOIN silver.erp_cust_az12 ca
 ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 lo
 ON ci.cst_key = lo.cid;
+
+-- =============================================================================
+-- Create Dimension: gold.dim_products
+-- =============================================================================
+DROP VIEW IF EXISTS gold.dim_products;
+
+CREATE VIEW gold.dim_products AS
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY pi.prd_start_dt, pi.prd_key) AS product_key,
+	pi.prd_id AS product_id,
+	pi.prd_key AS product_number,
+	pi.prd_nm AS product_name,
+	pi.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintenance,
+	pi.prd_cost AS cost,
+	pi.prd_line AS product_line,
+	pi.prd_start_dt AS start_date
+FROM
+(
+	SELECT prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt
+	FROM silver.crm_prd_info
+	WHERE prd_end_dt IS NOT NULL
+) AS pi LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pi.cat_id = pc.id;
+
+-- =============================================================================
+-- Create Dimension: gold.fact_sales
+-- =============================================================================
+DROP VIEW IF EXISTS gold.fact_sales;
+
+CREATE VIEW gold.fact_sales AS
+SELECT s.sls_ord_num AS order_number,
+p.product_key,
+c.customer_key,
+s.sls_order_dt AS order_date,
+s.sls_ship_dt AS ship_date,
+s.sls_due_dt AS due_date,
+s.sls_sales AS sales_amount,
+s.sls_quantity AS quantity,
+s.sls_price AS price
+FROM silver.crm_sales_details AS s
+LEFT JOIN gold.dim_products AS p
+ON s.sls_prd_key = p.product_number
+LEFT JOIN gold.dim_customers AS c
+ON s.sls_cust_id = c.customer_id;
